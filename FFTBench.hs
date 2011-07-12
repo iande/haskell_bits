a111d268a567b49413b4d6abe3d3e4102ad17753
Ian D. Eccles <ian.eccles@gmail.com>
2011-07-12 11:56:14 -0400

    show that we are getting the expected result from runFFT

--- FFTBench.hs
+++ FFTBench.hs
@@ -5,8 +5,8 @@ import Criterion.Config
 import Criterion.Main
 import Data.Complex
 
-runFft :: Integer -> [Complex Double] -> [Complex Double]
-runFft sz inps = let theta = (2.0 * pi / fromIntegral(sz))
+runFFT :: Integer -> [Complex Double] -> [Complex Double]
+runFFT sz inps = let theta = (2.0 * pi / fromIntegral(sz))
                      w     = conjugate $ cis theta
                      in fft sz w inps []
 
@@ -22,31 +22,35 @@ benchConfig = defaultConfig {
                 cfgSamples = ljust 1000
               }
 
-main = defaultMainWith benchConfig (return ()) [
-        bgroup "fft" [ bench "square wave" $ nf (runFft 16) squareWave,
-                       bench "saw-tooth wave" $ nf (runFft 16) sawToothWave
+main = do
+  putStrLn $ "Square Wave Result: " ++ (show $ runFFT 16 squareWave)
+  putStrLn $ "Saw-Tooth Wave Result: " ++ (show $ runFFT 16 sawToothWave)
+  defaultMainWith benchConfig (return ()) [
+        bgroup "fft" [ bench "square wave" $ nf (runFFT 16) squareWave,
+                       bench "saw-tooth wave" $ nf (runFFT 16) sawToothWave
                      ]
        ]
 
 {--
+Square Wave Result: [8.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,(-8.0) :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0]
+Saw-Tooth Wave Result: [24.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,(-8.0) :+ (-8.0),0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,8.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,(-7.999999999999999) :+ 8.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0]
 warming up
 estimating clock resolution...
-mean is 7.701010 us (80001 iterations)
-found 2521 outliers among 79999 samples (3.2%)
-  2169 (2.7%) high severe
+mean is 7.824400 us (80001 iterations)
+found 3390 outliers among 79999 samples (4.2%)
+  2989 (3.7%) high severe
 estimating cost of a clock call...
-mean is 67.06615 ns (76 iterations)
-found 12 outliers among 76 samples (15.8%)
-  4 (5.3%) high mild
-  8 (10.5%) high severe
+mean is 63.56386 ns (76 iterations)
+found 2 outliers among 76 samples (2.6%)
+  2 (2.6%) high mild
 
 benchmarking fft/square wave
-collecting 1000 samples, 44 iterations each, in estimated 7.774219 s
-mean: 181.1056 us, lb 180.4757 us, ub 181.9573 us, ci 0.950
-std dev: 11.78474 us, lb 9.528298 us, ub 14.98373 us, ci 0.950
+collecting 1000 samples, 45 iterations each, in estimated 7.936992 s
+mean: 179.4975 us, lb 179.0982 us, ub 180.0552 us, ci 0.950
+std dev: 7.503796 us, lb 5.733769 us, ub 9.836049 us, ci 0.950
 
 benchmarking fft/saw-tooth wave
-collecting 1000 samples, 44 iterations each, in estimated 7.713079 s
-mean: 178.9541 us, lb 178.5687 us, ub 179.4427 us, ci 0.950
-std dev: 6.982693 us, lb 5.837796 us, ub 8.591811 us, ci 0.950
+collecting 1000 samples, 44 iterations each, in estimated 7.920593 s
+mean: 180.3684 us, lb 179.8300 us, ub 181.1229 us, ci 0.950
+std dev: 10.17635 us, lb 7.919220 us, ub 13.15644 us, ci 0.950
 --}
