48f1e3c05ac4cd295a2dfdf5b218070299830e58
Ian D. Eccles <ian.eccles@gmail.com>
2011-07-12 12:07:24 -0400

    some explanation of benchmarks and output

--- FFTBench.hs
+++ FFTBench.hs
@@ -1,4 +1,4 @@
-module FFTBench where
+module Main where
 
 import FFT
 import Criterion.Config
@@ -23,8 +23,10 @@ benchConfig = defaultConfig {
               }
 
 main = do
-  putStrLn $ "Square Wave Result: " ++ (show $ runFFT 16 squareWave)
-  putStrLn $ "Saw-Tooth Wave Result: " ++ (show $ runFFT 16 sawToothWave)
+  putStr "Square Wave Result:\n\t"
+  putStrLn . show $ runFFT 16 squareWave
+  putStr "Saw-Tooth Wave Result:\n\t"
+  putStrLn . show $ runFFT 16 sawToothWave
   defaultMainWith benchConfig (return ()) [
         bgroup "fft" [ bench "square wave" $ nf (runFFT 16) squareWave,
                        bench "saw-tooth wave" $ nf (runFFT 16) sawToothWave
@@ -32,25 +34,42 @@ main = do
        ]
 
 {--
-Square Wave Result: [8.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,(-8.0) :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0]
-Saw-Tooth Wave Result: [24.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,(-8.0) :+ (-8.0),0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,8.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0,(-7.999999999999999) :+ 8.0,0.0 :+ 0.0,0.0 :+ 0.0,0.0 :+ 0.0]
+Results shown below come from compiling and running the benchmarks:
+
+      ghc -o fftbench FFTBench.hs
+      ./fftbench
+
+I have also formatted the resulting lists to make them a little prettier
+--}
+
+{--
+Square Wave Result:
+	[  8.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0,
+	   0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0,
+	 (-8.0) :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0,
+	   0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0]
+Saw-Tooth Wave Result:
+	[ 24.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0,
+	 (-8.0) :+ (-8.0), 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0,
+	   8.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0,
+	(-7.999999999999999) :+ 8.0, 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0]
 warming up
 estimating clock resolution...
-mean is 7.824400 us (80001 iterations)
-found 3390 outliers among 79999 samples (4.2%)
-  2989 (3.7%) high severe
+mean is 7.033964 us (80001 iterations)
+found 3086 outliers among 79999 samples (3.9%)
+  2291 (2.9%) high severe
 estimating cost of a clock call...
-mean is 63.56386 ns (76 iterations)
-found 2 outliers among 76 samples (2.6%)
-  2 (2.6%) high mild
+mean is 60.41778 ns (35 iterations)
+found 1 outliers among 35 samples (2.9%)
+  1 (2.9%) high mild
 
 benchmarking fft/square wave
-collecting 1000 samples, 45 iterations each, in estimated 7.936992 s
-mean: 179.4975 us, lb 179.0982 us, ub 180.0552 us, ci 0.950
-std dev: 7.503796 us, lb 5.733769 us, ub 9.836049 us, ci 0.950
+collecting 1000 samples, 204 iterations each, in estimated 7.065151 s
+mean: 34.04159 us, lb 33.99916 us, ub 34.09050 us, ci 0.950
+std dev: 732.3244 ns, lb 648.2252 ns, ub 986.0652 ns, ci 0.950
 
 benchmarking fft/saw-tooth wave
-collecting 1000 samples, 44 iterations each, in estimated 7.920593 s
-mean: 180.3684 us, lb 179.8300 us, ub 181.1229 us, ci 0.950
-std dev: 10.17635 us, lb 7.919220 us, ub 13.15644 us, ci 0.950
+collecting 1000 samples, 210 iterations each, in estimated 7.060356 s
+mean: 34.17536 us, lb 34.12706 us, ub 34.23696 us, ci 0.950
+std dev: 875.0766 ns, lb 731.1756 ns, ub 1.180891 us, ci 0.950
 --}
