59565904813ee55ea7d8b66b6929cb88efebdf44
Ian D. Eccles <ian.eccles@gmail.com>
2011-07-12 17:05:44 -0400

    updating benchmarks with some additional sample waves and results

--- FFTBench.hs
+++ FFTBench.hs
@@ -18,6 +18,27 @@ sawToothWave :: [Complex Double]
 sawToothWave = [1 :+ 0, 2 :+ 0, 3 :+ 0, 0 :+ 0, 1 :+ 0, 2 :+ 0, 3 :+ 0, 0 :+ 0,
                 1 :+ 0, 2 :+ 0, 3 :+ 0, 0 :+ 0, 1 :+ 0, 2 :+ 0, 3 :+ 0, 0 :+ 0]
 
+test1Wave :: [Complex Double]
+test1Wave    = [1 :+ 0, 0 :+ 0, 0 :+ 0, 0 :+ 0, 0 :+ 0, 0 :+ 0, 0 :+ 0, 0 :+ 0]
+-- expected: [1, 1, 1 ...]
+
+test2Wave :: [Complex Double]
+test2Wave    = [0 :+ 0, 1 :+ 0, 0 :+ 0, 0 :+ 0, 0 :+ 0, 0 :+ 0, 0 :+ 0, 0 :+ 0]
+{--
+Expected Result: (from Numeric.FFT.fft in pure-fft package)
+[1.0 :+ 0.0,
+ 0.7071067811865476 :+ (-0.7071067811865475),
+ 6.123233995736766e-17 :+ (-1.0),
+ (-0.7071067811865475) :+ (-0.7071067811865476),
+ (-1.0) :+ (-1.2246467991473532e-16),
+ (-0.7071067811865477) :+ 0.7071067811865475,
+ (-1.8369701987210297e-16) :+ 1.0,
+ 0.7071067811865475 :+ 0.7071067811865477]
+ 
+ We're close, but our "nearly 0" values are a bit larger than Numeric.FFT's
+ "nearly 0" values (e.g. 6.123233995736766e-17 vs 2.220446049250313e-16)
+--}
+
 benchConfig = defaultConfig {
                 cfgSamples = ljust 1000
               }
@@ -27,9 +48,15 @@ main = do
   putStrLn . show $ runFFT fft 16 squareWave
   putStr "Saw-Tooth Wave Result:\n\t"
   putStrLn . show $ runFFT fft 16 sawToothWave
+  putStr "Test1 Wave Result:\n\t"
+  putStrLn . show $ runFFT fft 8 test1Wave
+  putStr "Test2 Wave Result:\n\t"
+  putStrLn . show $ runFFT fft 8 test2Wave
   defaultMainWith benchConfig (return ()) [
         bgroup "fft" [ bench "square wave" $ nf (runFFT fft 16) squareWave,
-                       bench "saw-tooth wave" $ nf (runFFT fft 16) sawToothWave
+                       bench "saw-tooth wave" $ nf (runFFT fft 16) sawToothWave,
+                       bench "test1 wave" $ nf (runFFT fft 8) test1Wave,
+                       bench "test2 wave" $ nf (runFFT fft 8) test2Wave
                      ]
        ]
 
@@ -53,23 +80,46 @@ Saw-Tooth Wave Result:
 	(-8.0) :+ (-8.0), 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0,
 	8.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0,
 	(-7.999999999999999) :+ 8.0, 0.0 :+ 0.0, 0.0 :+ 0.0, 0.0 :+ 0.0]
+Test1 Wave Result:
+	[1.0 :+ 0.0, 1.0 :+ 0.0, 1.0 :+ 0.0, 1.0 :+ 0.0,
+	 1.0 :+ 0.0, 1.0 :+ 0.0, 1.0 :+ 0.0, 1.0 :+ 0.0]
+Test2 Wave Result:
+	[1.0 :+ 0.0,
+	 0.7071067811865476 :+ (-0.7071067811865475),
+	 2.220446049250313e-16 :+ (-1.0),
+	 (-0.7071067811865474) :+ (-0.7071067811865477),
+	 (-1.0) :+ 0.0,
+	 (-0.7071067811865476) :+ 0.7071067811865475,
+	 (-2.220446049250313e-16) :+ 1.0,
+	 0.7071067811865474 :+ 0.7071067811865477]
 warming up
 estimating clock resolution...
-mean is 6.990687 us (80001 iterations)
-found 2346 outliers among 79999 samples (2.9%)
-  1755 (2.2%) high severe
+mean is 8.010687 us (80001 iterations)
+found 11361 outliers among 79999 samples (14.2%)
+  5537 (6.9%) high mild
+  5824 (7.3%) high severe
 estimating cost of a clock call...
-mean is 59.62360 ns (36 iterations)
-found 1 outliers among 36 samples (2.8%)
-  1 (2.8%) high mild
+mean is 63.72107 ns (41 iterations)
+found 1 outliers among 41 samples (2.4%)
+  1 (2.4%) high mild
 
 benchmarking fft/square wave
-collecting 1000 samples, 232 iterations each, in estimated 7.019795 s
-mean: 30.44903 us, lb 30.42474 us, ub 30.47662 us, ci 0.950
-std dev: 415.8990 ns, lb 372.7239 ns, ub 479.0942 ns, ci 0.950
+collecting 1000 samples, 252 iterations each, in estimated 8.032378 s
+mean: 35.16367 us, lb 34.98511 us, ub 35.35924 us, ci 0.950
+std dev: 3.011818 us, lb 2.788621 us, ub 3.320808 us, ci 0.950
 
 benchmarking fft/saw-tooth wave
-collecting 1000 samples, 231 iterations each, in estimated 6.998573 s
-mean: 30.50131 us, lb 30.46343 us, ub 30.56316 us, ci 0.950
-std dev: 763.3206 ns, lb 552.3002 ns, ub 1.337548 us, ci 0.950
+collecting 1000 samples, 215 iterations each, in estimated 8.027195 s
+mean: 34.14124 us, lb 33.85232 us, ub 34.53818 us, ci 0.950
+std dev: 5.412538 us, lb 4.306758 us, ub 7.160480 us, ci 0.950
+
+benchmarking fft/test1 wave
+collecting 1000 samples, 471 iterations each, in estimated 8.026252 s
+mean: 15.32201 us, lb 15.25763 us, ub 15.42407 us, ci 0.950
+std dev: 1.279731 us, lb 933.2923 ns, ub 2.250423 us, ci 0.950
+
+benchmarking fft/test2 wave
+collecting 1000 samples, 540 iterations each, in estimated 8.021356 s
+mean: 15.95777 us, lb 15.88195 us, ub 16.04082 us, ci 0.950
+std dev: 1.280561 us, lb 1.194957 us, ub 1.390523 us, ci 0.950
 --}
